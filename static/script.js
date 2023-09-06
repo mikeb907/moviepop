4d6ff0105bb8160bcef086e6020de639d122db34
mikeb907 <mbattaglia92@gmail.com>
2023-09-06 13:21:17 -0400

    backup before integrating react

--- static/script.js
+++ static/script.js
@@ -4,92 +4,99 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const homeMenu = document.getElementById('home-menu');
     const gameScreen = document.getElementById('game-screen');
-    const optionBtns = document.querySelectorAll('.option-btn');
+    const startGameBtn = document.getElementById('start-game');
+    const draggableOptions = document.querySelectorAll('.draggable');
+    const dropZones = document.querySelectorAll('.drop-zone');
+    const movieGroups = document.querySelectorAll('.movie-group');
+    const prevArrowBtn = document.querySelector('.prev-arrow-btn');
+    const nextArrowBtn = document.querySelector('.next-arrow-btn');
     const submitAnswersBtn = document.getElementById('submit-answers-btn');
-    const stillIndicator = document.getElementById('still-indicator');
-    const prevArrowButton = document.querySelector('.prev-arrow-btn');
-    const nextArrowButton = document.querySelector('.next-arrow-btn');
 
-    fetch('/get-movies')
-        .then(response => response.json())
-        .then(data => {
-            console.log("Received movies data:", data);
-        })
-        .catch(error => {
-            console.error('Error fetching movies:', error);
-        });
+    let draggedItem = null;
 
-    document.getElementById('play-btn').addEventListener('click', function() {
+    startGameBtn.addEventListener('click', function() {
         homeMenu.style.display = 'none';
         gameScreen.style.display = 'block';
-        showMovieGroup(currentMovieNumber);
-        updateStillIndicator();
+        updateStillsDisplay();
     });
 
-    optionBtns.forEach(btn => {
-        btn.addEventListener('click', function() {
-            const selectedOptionNumber = parseInt(btn.getAttribute('data-option-number'));
-        
-            // Check if this option was previously selected for another still
-            const previousIndex = userAnswers.indexOf(selectedOptionNumber);
-            if (previousIndex !== -1) {
-                userAnswers[previousIndex] = null;
-            }
-        
-            // Update the answer for this still
-            userAnswers[currentMovieNumber - 1] = selectedOptionNumber;
-        
-            // Remove any previous still number from all titles
-            optionBtns.forEach(option => {
-                const existingSpan = option.querySelector('.still-number');
-                if (existingSpan) existingSpan.remove();
-            });
-            
-            // Place the new still number next to the selected titles
-            for (let i = 0; i < userAnswers.length; i++) {
-                if (userAnswers[i] !== null) {
-                    const chosenTitles = document.querySelectorAll(`.option-btn[data-option-number="${userAnswers[i]}"]`);
-                    chosenTitles.forEach(chosenTitle => {
-                        // Only add a number if it doesn't already exist
-                        if (!chosenTitle.querySelector('.still-number')) {
-                            const span = document.createElement('span');
-                            span.className = 'still-number';
-                            span.innerText = ` ${i + 1}`;
-                            chosenTitle.appendChild(span);
-                        }
-                    });
-                }
-            }
-        
+    function dragStartFunction(e) {
+        draggedItem = e.target;
+        setTimeout(() => {
+            e.target.style.opacity = '0';
+        }, 0);
+    }
+
+    function dragEndFunction(e) {
+        setTimeout(() => {
+            draggedItem.style.opacity = '1';
+            draggedItem = null;
+        }, 0);
+    }
+
+    draggableOptions.forEach(option => {
+        option.addEventListener('dragstart', dragStartFunction);
+        option.addEventListener('dragend', dragEndFunction);
+    });
+
+    dropZones.forEach(zone => {
+        zone.addEventListener('dragover', function(e) {
+            e.preventDefault();
+        });
+
+        zone.addEventListener('dragenter', function(e) {
+            e.preventDefault();
+        });
+
+        zone.addEventListener('drop', function(e) {
+            if (!draggedItem) return;
+
+            const movieNumber = parseInt(e.target.getAttribute('data-movie-number'));
+            userAnswers[movieNumber - 1] = parseInt(draggedItem.getAttribute('data-option-number'));
+
+            // Update UI
+            e.target.innerText = draggedItem.innerText;
+            e.target.setAttribute('data-option-number', draggedItem.getAttribute('data-option-number')); // Store the original option number
+            draggedItem.style.display = 'none'; // Hide the dragged item
+
+            checkAllAnswersAssigned();
+        });
+
+        // Allow a title to be dragged back to the options
+        zone.addEventListener('dragstart', dragStartFunction);
+        zone.addEventListener('dragend', function(e) {
+            if (!draggedItem) return;
+
+            const optionNumber = parseInt(draggedItem.getAttribute('data-option-number'));
+            document.querySelector(`.draggable[data-option-number="${optionNumber}"]`).style.display = 'block';
+            draggedItem.innerText = '';
+            draggedItem.removeAttribute('data-option-number');
             checkAllAnswersAssigned();
         });
     });
 
-    prevArrowButton.addEventListener('click', function() {
-        currentMovieNumber--;
-        if (currentMovieNumber < 1) {
-            currentMovieNumber = 3;
+    prevArrowBtn.addEventListener('click', function() {
+        if (currentMovieNumber > 1) {
+            currentMovieNumber--;
+            updateStillsDisplay();
         }
-        showMovieGroup(currentMovieNumber);
-        updateStillIndicator();
     });
 
-    nextArrowButton.addEventListener('click', function() {
-        currentMovieNumber++;
-        if (currentMovieNumber > 3) {
-            currentMovieNumber = 1;
+    nextArrowBtn.addEventListener('click', function() {
+        if (currentMovieNumber < 3) {
+            currentMovieNumber++;
+            updateStillsDisplay();
         }
-        showMovieGroup(currentMovieNumber);
-        updateStillIndicator();
     });
 
-    function showMovieGroup(number) {
-        document.querySelectorAll('.movie-group').forEach(group => group.style.display = 'none');
-        document.querySelector(`[data-movie-number="${number}"]`).style.display = 'block';
-    }
-
-    function updateStillIndicator() {
-        stillIndicator.textContent = `${currentMovieNumber}/3`;
+    function updateStillsDisplay() {
+        movieGroups.forEach(group => {
+            if (parseInt(group.getAttribute('data-movie-number')) === currentMovieNumber) {
+                group.classList.add('active');
+            } else {
+                group.classList.remove('active');
+            }
+        });
     }
 
     function checkAllAnswersAssigned() {
@@ -99,4 +106,6 @@ document.addEventListener('DOMContentLoaded', function() {
             submitAnswersBtn.setAttribute('disabled', 'disabled');
         }
     }
+
+    // TODO: Add logic for submitting and checking answers.
 });
