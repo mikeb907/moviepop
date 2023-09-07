2785de1d03027a394189519ab7c05be75dee40de
mikeb907 <mbattaglia92@gmail.com>
2023-09-07 15:26:29 -0400

    js responsive

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeMenu from '../HomeMenu/HomeMenu';
-import GameScreen from '../GameScreen/GameScreen';  // Import the GameScreen component
+import GameScreen from '../GameScreen/GameScreen';
 
 function App() {
+
+    // Use the useEffect hook to run the function once the component mounts
+    useEffect(() => {
+        // Define the function that sets the custom --vh CSS variable
+        function setVHProperty() {
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+
+        // Set the --vh value when the component mounts
+        setVHProperty();
+
+        // Update the --vh value whenever the window is resized
+        window.addEventListener('resize', setVHProperty);
+
+        // Cleanup: remove the event listener when the component is unmounted
+        return () => {
+            window.removeEventListener('resize', setVHProperty);
+        };
+    }, []); // Empty dependency array ensures this useEffect runs only once when the component mounts
+
     return (
-        <Router> {/* Wrap the entire application in a Router */}
+        <Router>
             <div className="App">
                 <Routes>
                     <Route path="/" element={<HomeMenu />} />
