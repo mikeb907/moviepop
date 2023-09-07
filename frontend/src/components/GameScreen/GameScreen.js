ef72f5a65c22ebaaa05dd3ed74ea33e86552842b
mikeb907 <mbattaglia92@gmail.com>
2023-09-07 18:37:03 -0400

    Score submit states, responsive styling, sharing functionality

--- frontend/src/components/GameScreen/GameScreen.js
+++ frontend/src/components/GameScreen/GameScreen.js
@@ -34,6 +34,40 @@ function GameScreen() {
 
     const [currentStillIndex, setCurrentStillIndex] = useState(0);
     const [selectedTitles, setSelectedTitles] = useState([]);
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    
+    const gameNumber = 1;  // This is just an example, replace with your actual logic
+    const [emojiPop, setEmojiPop] = useState(false); // New state for emoji pop effect
+
+    const handleSubmission = () => {
+        setHasSubmitted(true);
+        setTimeout(() => setEmojiPop(true), 100); // Delay the pop effect slightly for a fun effect
+    };
+
+    const getEmojiScore = () => {
+        return selectedTitles.map((title, index) => {
+            return title === stillsData[index].correctTitle ? '🍿' : '🍅';
+        }).join(' ');
+    };
+
+    const handleShare = () => {
+        const numberCorrect = selectedTitles.filter((title, index) => title === stillsData[index].correctTitle).length;
+        const emojiScore = getEmojiScore();
+        
+        if (navigator.share) {
+            navigator.share({
+                title: 'My MoviePop Results',
+                text: `MoviePop #${gameNumber} • ${numberCorrect}/3\n\n${emojiScore}`,
+                // Add a URL if you want to share a link too (optional)
+                // url: 'https://your-quiz-url.com',
+            })
+            .then(() => console.log('Successful share'))
+            .catch((error) => console.log('Error sharing', error));
+        } else {
+            // Fallback for browsers that don't support the Web Share API
+            alert("Your browser doesn't support the Web Share API.");
+        }
+    };
 
     const selectTitle = (title) => {
         const updatedSelections = [...selectedTitles];
@@ -48,51 +82,86 @@ function GameScreen() {
         setSelectedTitles(updatedSelections);
     };
 
+    const getButtonClass = (title) => {
+        if (!hasSubmitted) {
+            return selectedTitles[currentStillIndex] === title ? 'selected' : '';
+        }
+        
+        if (title === stillsData[currentStillIndex].correctTitle) {
+            return 'correct';
+        } else if (selectedTitles[currentStillIndex] === title) {
+            return 'incorrect';
+        }
+        return '';
+    };
+
     return (
         <div className="game-screen">
             <Navbar />
 
-            <div class="question-content-container">
-
+            <div className="question-content-container">
                 <div className="movie-still-container">
                     <div className="title-bar">
                         <h2>Movie {currentStillIndex + 1}/3</h2>
-                        <div className="circle-indicators">
-                            {stillsData.map((_, index) => (
-                                <div 
-                                    key={index} 
-                                    className={`circle ${selectedTitles[index] ? 'answered' : ''}`}
-                                    onClick={() => setCurrentStillIndex(index)}  // <-- This is the new line
-                                />
-                            ))}
-                        </div>
-
                     </div>
 
                     <div className="movie-still">
                         <img src={stillsData[currentStillIndex].image} alt={`Movie Still ${currentStillIndex + 1}`} />
                     </div>
-                    
                 </div>
-                
+
                 <div className="title-choices">
                     {stillsData[currentStillIndex].options.map((title, index) => (
                         <button 
                             key={index} 
-                            className={`title-choice-button ${selectedTitles[currentStillIndex] === title ? 'selected' : ''}`} 
+                            className={`title-choice-button ${getButtonClass(title)}`} 
                             onClick={() => selectTitle(title)}
+                            disabled={hasSubmitted}
                         >
                             {title}
                         </button>
                     ))}
                 </div>
             </div>
-            
+
+            <div className="circle-indicators">
+                {stillsData.map((still, index) => {
+                    const isAnswered = selectedTitles[index];
+                    const isCorrect = selectedTitles[index] === still.correctTitle;
+                    return (
+                        <div key={index} className={hasSubmitted ? 'answered-container' : ''} onClick={() => setCurrentStillIndex(index)}>
+                            <div 
+                                className={`circle ${isAnswered ? 'answered' : ''} ${hasSubmitted ? 'submitted' : ''}`}
+                            />
+                            {hasSubmitted && (
+                                <span className={`emoji ${emojiPop ? 'emoji-pop' : ''}`}>
+                                    {isCorrect ? '🍿' : '🍅'}
+                                </span>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
+
             <div className="action-bar">
                 <button className="prev-arrow" onClick={() => setCurrentStillIndex(prev => Math.max(prev - 1, 0))}>
                     <ArrowBackIosIcon />
                 </button>
-                <button className={`submit-btn ${selectedTitles.length === 3 && !selectedTitles.includes(null) ? 'active' : ''}`} disabled={selectedTitles.length < 3 || selectedTitles.includes(null)}>Submit</button>
+
+                {!hasSubmitted ? (
+                    <button 
+                        className={`submit-btn ${selectedTitles.length === 3 && !selectedTitles.includes(null) ? 'active' : ''}`} 
+                        onClick={handleSubmission}
+                        disabled={selectedTitles.length < 3 || selectedTitles.includes(null)}
+                    >
+                        Submit
+                    </button>
+                ) : (
+                    <button className="submit-btn share-btn" onClick={handleShare}>
+                        Share
+                    </button>
+                )}
+
                 <button className="next-arrow" onClick={() => setCurrentStillIndex(prev => Math.min(prev + 1, stillsData.length - 1))}>
                     <ArrowForwardIosIcon />
                 </button>
--- frontend/src/components/GameScreen/Navbar/Navbar.js
+++ frontend/src/components/GameScreen/Navbar/Navbar.js
@@ -3,10 +3,12 @@
 import React from 'react';
 import './Navbar.css';
 import { useNavigate } from 'react-router-dom';  
-import CloseIcon from '@material-ui/icons/Close';
+// import CloseIcon from '@material-ui/icons/Close';
 import HelpIcon from '@material-ui/icons/Help';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import SettingsIcon from '@material-ui/icons/Settings';
+import logo from '../../../assets/moviepop-logo.png';  // Assuming you placed the image in an 'assets' folder
+
 
 function Navbar() {
     const navigate = useNavigate();  // Instantiate the navigate hook
@@ -17,7 +19,8 @@ function Navbar() {
 
     return (
         <div className="navbar">
-            <CloseIcon className="close-btn" onClick={navigateToHome} />  {/* Attach click handler */}
+            {/* <CloseIcon className="close-btn" onClick={navigateToHome} />   */}
+            <img className="close-btn" onClick={navigateToHome} src={logo} alt="MoviePop Logo" />
             <div className="right-icons">
                 <HelpIcon />
                 <BarChartIcon />
