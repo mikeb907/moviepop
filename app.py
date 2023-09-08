bbeda95b8e7d8349729f4ecfcbe92711d8c27657
mikeb907 <mbattaglia92@gmail.com>
2023-09-08 12:33:42 -0400

    Prepare for Heroku deployment

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import random
 
 app = Flask(__name__, static_folder="frontend/build/static", template_folder="frontend/build")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+
+# Initialize CORS with the app
+CORS(app)
 
 db = SQLAlchemy(app)
 
@@ -16,8 +20,6 @@ class Movie(db.Model):
 def index():
     return send_from_directory('frontend/build', 'index.html')
 
-
-
 @app.route('/get-movies')
 def get_movies():
     movies = Movie.query.all()
@@ -27,7 +29,9 @@ def get_movies():
     movies_data = [{"title": movie.title, "still_url": movie.still_url} for movie in movies]
     return {"movies": random.sample(movies_data, 3)}  # Randomly selecting 3 movies
 
+# Move database initialization outside the block below
+with app.app_context():
+    db.create_all()  # Creates the movies table if it doesn't exist yet
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Creates the movies table if it doesn't exist yet
     app.run(host='127.0.0.1', port=5000, debug=True, threaded=True)
