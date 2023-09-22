fe0e08b68502ba27ee3aab5a728dc38f9d4a4d09
mikeb907 <mbattaglia92@gmail.com>
2023-09-22 10:39:19 -0400

    universal redis update

--- app.py
+++ app.py
@@ -29,6 +29,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # # Setup Redis
 
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+
+env = os.environ.get('FLASK_ENV', 'development')
+
+if env == 'staging':
+    # Use the Redis connection setup that works for staging
+    redis_conn = redis.from_url(redis_url, ssl_cert_reqs=ssl.CERT_NONE)
+elif env == 'production':
+    # Use the Redis connection setup that works for production
+    url = urlparse(redis_url)
+    if url.scheme == 'rediss':  # Indicates SSL
+        pool = redis.ConnectionPool(
+            host=url.hostname,
+            port=url.port,
+            password=url.password,
+            ssl=True,
+            ssl_cert_reqs=ssl.CERT_NONE
+        )
+    else:
+        pool = redis.ConnectionPool(
+            host=url.hostname,
+            port=url.port,
+            password=url.password
+        )
+    redis_conn = redis.Redis(connection_pool=pool)
+else:
+    # Default case for development
+    redis_conn = redis.Redis(host='localhost', port=6379)
+
 
 # redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 # url = urlparse(redis_url)
@@ -50,9 +79,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # redis_conn = redis.Redis(connection_pool=pool)
 # q = Queue(connection=redis_conn)
 
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
-redis_conn = redis.from_url(redis_url, ssl_cert_reqs=ssl.CERT_NONE)
-q = Queue(connection=redis_conn)
+# redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+# redis_conn = redis.from_url(redis_url, ssl_cert_reqs=ssl.CERT_NONE)
+# q = Queue(connection=redis_conn)
 
 
 
